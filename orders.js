f17e286f43418915119e9b5ca40a40271876fc43
PEEYOOSH SURYAWANSHI <118802718+PeeyooshMS@users.noreply.github.com>
2023-06-15 12:27:49 +0530

    feat : table data fetched from external Js file

---
+++ orders.js
@@ -0,0 +1,30 @@
+const Orders = [
+    {
+        invoiceNo : '140612',
+        customerName : 'Shubh Joshi',
+        serviceType : 'Cultivation',
+        paymentStatus : 'Due',
+        serviceStatus : 'Pending'
+    },
+    {
+        invoiceNo : '140627',
+        customerName : 'Nikhil Nayar',
+        serviceType : 'Plough',
+        paymentStatus : 'Partial',
+        serviceStatus : 'Delivered'
+    },
+    {
+        invoiceNo : '140619',
+        customerName : 'Dhananjay Rajpoot',
+        serviceType : 'Seeding',
+        paymentStatus : 'Complete',
+        serviceStatus : 'Delivered'
+    },
+    {
+        invoiceNo : '140620',
+        customerName : 'Lolark Yadav',
+        serviceType : 'Cultivation',
+        paymentStatus : 'Due',
+        serviceStatus : 'Delivered'
+    }
+]
